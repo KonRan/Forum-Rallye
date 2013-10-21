f3b8f647a89c3d6b5e7fc9a665834cf26b4bd13c
KonRan <timothe.bournay@gmail.com>
2013-10-21 18:46:16 +0200

    Vérif git

--- src/com/forum_rallye/MainActivity.java
+++ src/com/forum_rallye/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 
     @Override
-    //test
     //test de commentaire
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
