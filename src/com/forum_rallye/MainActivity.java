a3c2117e78a9dafdaec690fe6997456de8a1bb77
KonRan <timothe.bournay@gmail.com>
2013-10-22 09:48:25 +0200

    Btn quitter & lien fofo

--- src/com/forum_rallye/MainActivity.java
+++ src/com/forum_rallye/MainActivity.java
@@ -1,8 +1,15 @@
 package com.forum_rallye;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends Activity {
 
@@ -11,6 +18,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btn1 = (Button) findViewById(R.id.ButtonQuit);
+        btn1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                System.exit(0);
+            }
+        });
+        
+        ImageView img = (ImageView) findViewById(R.id.imageForum);
+        img.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+            	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.forum-rallye.com"));
+            	startActivity(intent);            }
+        });
     }
 
 
